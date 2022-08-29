e83757b04cee2d257ff64c0168cdbb7f7dca6a8b
hel99 <helena.aganovic99@gmail.com>
2022-08-29 20:22:48 +0200

    Budzet.jsx strana, prikaz trenutnog stanja

---
+++ src/page/Budzet.jsx
@@ -0,0 +1,12 @@
+
+function Budzet() {
+
+    return (
+        <div>
+            <h1 className="h1-n">Moj budžet</h1>
+            <h1 className="h1-n">Iznos:</h1>
+        </div>
+    )
+}
+
+export default Budzet;
